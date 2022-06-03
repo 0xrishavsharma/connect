1705c7d7aea3083c3e3c9abc9691689f8d20bd5e
Rishav Sharma <rishabhtonic2000@gmail.com>
2022-06-03 17:38:57 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { recommendedProfiles, client } from "../api";
+
+export default function Home() {
+  useEffect( ()=> {
+    fetchProfiles()
+  }, [])
+
+  function fetchProfiles(){
+    const request = client.query(recommendedProfiles).toPromise;
+  }
+  return (
+  
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
+          </span>
+        </a>
+      </footer>
+  )
+}
