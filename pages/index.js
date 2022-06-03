62044b10a1f13ec52fe4dea6f4f0d180e4e9563e
Rishav Sharma <rishabhtonic2000@gmail.com>
2022-06-03 17:41:03 +0530

    first

--- pages/index.js
+++ pages/index.js
@@ -11,14 +11,14 @@ export default function Home() {
   }
   return (
   
-      <footer className={styles.footer}>
+      <footer>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <span className={styles.logo}>
+          <span >
             {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
           </span>
         </a>
